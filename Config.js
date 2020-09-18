0b2fd0ca835cc0e3169eadf1b04c76ed6d44fcb8
cyubuchen <cyubuchen@gmail.com>
2020-09-18 21:10:15 +0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: rss-square;
 /*
  * Author: cyubuchen
  * Github: https://github.com/cyubuchen/scripts
